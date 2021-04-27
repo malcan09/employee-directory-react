f2bdce0bb1e9a35c242dc717d420c962d7330350
Miguel Alcantar <malcantar185@gmail.com>
2021-04-26 21:13:04 -0700

    files

---
+++ employee/src/App.js
@@ -0,0 +1,15 @@
+import Table from "./components/Table";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Employee Directory</h1>
+      </header>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ employee/src/components/Table/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+import SearchForm from "../SearchForm";
+
+class Table extends Component{
+
+    render(){
+        return (
+            <div>
+                This is a table
+            </div>
+        )
+    }
+}
+export default Table;
